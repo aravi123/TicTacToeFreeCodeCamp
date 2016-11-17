774dc97aa3f8b4c5dc0bef38358e875bbd8e6c4b
aravi123 <aravindcranjan54@gmail.com>
2016-11-17 22:19:17 +0530

    Final Product

--- index.js
+++ index.js
@@ -3,19 +3,34 @@ $(document).ready(function(){
 	var x = 'X';
 	var o = '0';
 	var click = [],m=0;
+	var pos=0,d=0;
+	var flag=0;
+	var check =[],u=[],l=0;
 	var wins = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 	//console.log(wins.length);
-	var userturns = [],d=0;
+	var userturns = [],u=0,d=0;
+	var comp =[],g=0;
+	$('.X').click(function(){
+		$('.Game').show();
+		$('.Values').hide();
+	});
+	$('.O').click(function(){
+		$('.Game').show();
+		$('.Values').hide();
+		x = '0';
+		o = 'X';
+		analysegame();
 
+	});
 	$('li').click(function(){
 		var clickedelement = $(this).attr('class');
 		if (turns%2==0) {
 			$(this).html(x);
 			userturns[d++] = clickedelement;
 			click[m++]= clickedelement;
+			gameover(userturns);
 			analysegame(clickedelement);
-			//computerturn();
-			//check =[];
+			
 		}
 		//console.log(turns);
 		console.log(check);
@@ -23,59 +38,81 @@ $(document).ready(function(){
 		turns++;
 	});
 	var computerturns = [];
-	var check =[],u=[],l=0;
 	function analysegame(clickedelement){
 		for (var i = 0; i < wins.length; i++) {
 			for (var j = 0; j < wins[i].length; j++) {
-				//wins[i][i]
 				if (wins[i][j]==clickedelement) {
 					check.push(i);
 				}
 			}
 		}
-		var pos=0,d=0;
+		console.log(check);
 		for (var i = 0; i < check.length; i++) {
-			for (var j = i+1; j < check.length-1; j++) {
+			for (var j = i+1; j < check.length; j++) {
 				if ((check[j]==check[i])&&((click.indexOf(wins[check[i]][0])==-1)||(click.indexOf(wins[check[i]][1])==-1)||(click.indexOf(wins[check[i]][2])==-1))) {
 					pos = check[i];
 				}
 			}
 		} 
 		console.log(pos);
-		
-		
+
 		if(click.indexOf(String(wins[pos][0]))==-1){
-			console.log("obj1");
 			$('.'+wins[pos][0]).html(o);
+			comp[g++] = wins[pos][0];
 			click[m++] = String(wins[pos][0]);
+			check.splice(check.indexOf(pos),1);
+			gameover(comp);
 		}
 		else if (click.indexOf(String(wins[pos][1]))==-1) {
-			console.log("obj2");
 			$('.'+wins[pos][1]).html(o);
+			comp[g++] = wins[pos][1];
 			click[m++] = String(wins[pos][1]);
+			check.splice(check.indexOf(pos),1);
+			gameover(comp);
 		}
 		else if(click.indexOf(String(wins[pos][2]))==-1){
-			console.log("obj3");
 			$('.'+wins[pos][2]).html(o);
+			comp[g++] = wins[pos][2];
 			click[m++] = String(wins[pos][2]);
+			check.splice(check.indexOf(pos),1);
+			gameover(comp);
 		}
 		else {
-			var d =0;
+			var d =0,e=0;
 			for (var i = 0; i < wins.length; i++) {
 				for (var j = 0; j < wins[i].length; j++) {
 					if(click.indexOf(String(wins[i][j]))==-1){
 						d=wins[i][j];
+						e=i;
 					}
 				}
 			}
 			if (d!=0) {
 				$('.'+d).html(o);
+				comp[g++] = d;
 				click[m++] = String(d);
+				check.splice(check.indexOf(e),1);
+				gameover(comp);
 			}
 		}	
 		turns++;
-
-
+	}	
+	function gameover(arr) {
+		var flag=0,i=0;
+		for (var j = 0; j < wins.length; j++) {
+			for (var i = 0; i < arr.length; i++) {
+				if (arr[i]==wins[j][flag]) {
+				flag++;
+				i=0;
+				}
+			}
+			if (flag==3) {
+				console.log("game over");
+				location.reload();
+				break;
+			}
+			flag=0;
+		}
 	}
 
 });
